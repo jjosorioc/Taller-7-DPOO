7d1026862479304511bf847bcc5c886cf85212f3
marianadiaz179 <m.diaza2@uniandes.edu.co>
2021-12-01 21:18:41 -0500

    Intento pruebas Categoria

--- Taller 7 - Pruebas/src/uniandes/cupi2/pruebas/PruebasCategoria.java
+++ Taller 7 - Pruebas/src/uniandes/cupi2/pruebas/PruebasCategoria.java
@@ -2,13 +2,86 @@ package uniandes.cupi2.pruebas;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class PruebasCategoria {
+import uniandes.cupi2.almacen.mundo.AlmacenException;
+import uniandes.cupi2.almacen.mundo.Categoria;
+import uniandes.cupi2.almacen.mundo.NodoAlmacen;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+class PruebasCategoria {
+	
+	private Categoria categoria;
+	public Categoria nuevaCategoria;
+	
+	@BeforeEach
+	public void setup() throws AlmacenException
+	{
+		categoria = new Categoria ("Categoria", "Categoria");
+		nuevaCategoria = new Categoria ("Categoria1","Categoria1");
+		categoria.agregarNodo(categoria.darIdentificador(),nuevaCategoria);
 	}
 
+	@Test
+	public void testAgregarNodo() {
+		
+		assertEquals(categoria.darNodos().size(),1);
+		
+		}
+	
+	@Test
+	public void testBuscarNodo() {
+		
+		NodoAlmacen buscada = categoria.buscarNodo(nuevaCategoria.darIdentificador());
+		assertEquals(buscada,nuevaCategoria);
+		
+	}
+	
+	@Test
+	public void testDarValorVentas() {
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEliminarNodo() {
+		
+		categoria.eliminarNodo(nuevaCategoria.darIdentificador());
+		assertFalse(categoria.darNodos().contains(nuevaCategoria));
+		
+	}
+	
+	@Test
+	public void testBuscarProducto() {
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testBuscarPadre() {
+		
+		Categoria padre = categoria.buscarPadre(nuevaCategoria.darIdentificador());
+		assertEquals(categoria,padre);
+		
+	}
+	
+	@Test
+	public void testDarProductos() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDarMarcas() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDarPreorden() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDarPosorden() {
+		fail("Not yet implemented");
+	}
 }
